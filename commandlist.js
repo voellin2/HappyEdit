d74666f0958c96003b9650de6c88d6d17454fdc3
Per Thulin <per@youtify.com>
2012-10-30 19:17:16 +0100

    Fix typo

--- commandlist.js
+++ commandlist.js
@@ -27,7 +27,7 @@ function CommandList(happyEdit) {
         },
         {
             name: "stackoverflow",
-            title: "Search StackOverflow",
+            title: "Search Stack Overflow",
             hideCommandLine: false,
             autoComplete: function(s) {
                 happyEdit.stackOverflow.fillCommandLineWithAutoCompletions(s);
