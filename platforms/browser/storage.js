9fe11cdd7514ef38c805173b339368dc8bfc9716
Per Thulin <per@youtify.com>
2012-12-02 21:56:37 +0100

    Remove unnecessary capability checking
    When compiled as a Chrome Packaged App, we know
    Chrome storage is available.

--- platforms/browser/storage.js
+++ platforms/browser/storage.js
@@ -3,23 +3,14 @@
  */
 var Storage = {
     get: function(key, defaultValue, callback) {
-        if (window.localStorage) {
-            if (key in window.localStorage) {
-                callback(JSON.parse(window.localStorage[key]));
-            } else {
-                callback(defaultValue);
-            }
+        if (key in window.localStorage) {
+            callback(JSON.parse(window.localStorage[key]));
         } else {
-            console.log('localStorage is not available');
             callback(defaultValue);
         }
     },
 
     set: function(key, val, callback) {
-        if (window.localStorage) {
-            localStorage[key] = JSON.stringify(val);
-        } else {
-            console.log('localStorage is not available');
-        }
+        localStorage[key] = JSON.stringify(val);
     }
 }
--- platforms/chrome/storage.js
+++ platforms/chrome/storage.js
@@ -3,35 +3,26 @@
  */
 var Storage = {
     get: function(key, defaultValue, callback) {
-        if (chrome && chrome.storage && chrome.storage.local) {
-            return chrome.storage.local.get(key, function(items) {
-                if (items.hasOwnProperty(key)) {
-                    callback(items[key]);
-                } else {
-                    callback(defaultValue);
-                }
-            });
-        } else {
-            console.log('chrome.storage.local is not available');
-            callback(defaultValue);
-        }
+        return chrome.storage.local.get(key, function(items) {
+            if (items.hasOwnProperty(key)) {
+                callback(items[key]);
+            } else {
+                callback(defaultValue);
+            }
+        });
     },
 
     set: function(key, val, callback) {
-        if (chrome && chrome.storage && chrome.storage.local) {
-            var items = {};
-            items[key] = val;
+        var items = {};
+        items[key] = val;
 
-            // TODO research why inlining the items var in the following
-            // function call does not work.
+        // TODO research why inlining the items var in the following
+        // function call does not work.
 
-            chrome.storage.local.set(items, function() {
-                if (callback) {
-                    callback();
-                }
-            });
-        } else {
-            console.log('chrome.storage.local is not available');
-        }
+        chrome.storage.local.set(items, function() {
+            if (callback) {
+                callback();
+            }
+        });
     }
 }
