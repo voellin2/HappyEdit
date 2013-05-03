31b149fddbd7a06bdcd6eab5e3dd6f189ada60be
Per Thulin <per@youtify.com>
2013-05-03 22:41:03 +0200

    Tidy

--- js/Notifications.js
+++ js/Notifications.js
@@ -1,11 +1,10 @@
-function Notifications() {
+function Notifications(happyEdit) {
     var self = this;
     self.$notification = document.querySelector('#notification');
     
     self.show = function(msg) {
         self.$notification.innerHTML = Utils.htmlEscape(msg);
         self.$notification.style.visibility = 'visible';
-        
     };
     
     self.hide = function() {
